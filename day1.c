30c5fd008e2b871a0bebbb263aab1c55c74a6002
Arne Dußin <arne.dussin@gmail.com>
2022-12-04 11:31:30 +0100

    Fix formatting of day 1 code

--- day1.c
+++ day1.c
@@ -4,25 +4,25 @@
 #include <errno.h>
 #include <inttypes.h>
 #include <stdbool.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void shift_elements_left(u32 *array, size_t pos)
 {
     for (size_t i = 0; i < pos; ++i) {
-	array[i] = array[i+1];
+        array[i] = array[i + 1];
     }
 }
 
 void insert_into_max_array(u32 new, u32 *maximums, size_t len)
 {
     for (size_t i = 0; i < len; ++i) {
-	if (new > maximums[i]) {
-	    shift_elements_left(maximums, i);
-	    maximums[i] = new;
-	}
+        if (new > maximums[i]) {
+            shift_elements_left(maximums, i);
+            maximums[i] = new;
+        }
     }
 }
 
@@ -34,22 +34,22 @@ i64 read_next_elfs_calories(FILE *file)
 
     u32 sum = 0;
     while ((read = getline(&line, &line_len, file)) != -1) {
-	// Empty line denominates the next elf
-	if (strncmp(line, "\n", read) == 0) {
-	    free(line);
-	    return sum;
-	}
+        // Empty line denominates the next elf
+        if (strncmp(line, "\n", read) == 0) {
+            free(line);
+            return sum;
+        }
 
-	const char *trimmed = strtrim(line, (size_t *) &read);
-	uintmax_t cal = strtoumax(trimmed, NULL, 10);
-	if (cal == UINTMAX_MAX) {
-	    printf("Error parsing calories. %d", errno);
-	    free(line);
-	    line = NULL;
-	    return -2;
-	}
+        const char *trimmed = strtrim(line, (size_t *) &read);
+        uintmax_t cal = strtoumax(trimmed, NULL, 10);
+        if (cal == UINTMAX_MAX) {
+            printf("Error parsing calories. %d", errno);
+            free(line);
+            line = NULL;
+            return -2;
+        }
 
-	sum += cal;
+        sum += cal;
     }
 
     free(line);
@@ -60,16 +60,16 @@ int main()
 {
     FILE *file = fopen("input/day1.txt", "r");
     if (file == NULL) {
-	printf("Unable to open input file");
-	return 1;
+        printf("Unable to open input file");
+        return 1;
     }
 
     u32 max = 0;
     u32 max_array[3] = {0, 0, 0};
     i64 current;
     while ((current = read_next_elfs_calories(file)) != -1) {
-	max = u32_max(max, current);
-	insert_into_max_array(current, max_array, 3);
+        max = u32_max(max, current);
+        insert_into_max_array(current, max_array, 3);
     }
 
     printf("Maximum number of calories: %d kcal\n", max);
@@ -77,10 +77,11 @@ int main()
     // Add three highest together
     u32 max_sum = 0;
     for (u8 i = 0; i < 3; ++i) {
-	max_sum += max_array[i];
+        max_sum += max_array[i];
     }
 
-    printf("Three top calory carriers' calories compounded: %d kcal\n", max_sum);
+    printf("Three top calory carriers' calories compounded: %d kcal\n",
+           max_sum);
 
     return 0;
 }
