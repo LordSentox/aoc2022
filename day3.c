0eeed2a9adb43c156c1927202e1ec0e8859875bb
Arne Dußin <arne.dussin@gmail.com>
2022-12-04 12:14:03 +0100

    Fix formatting of day 3

--- day3.c
+++ day3.c
@@ -3,31 +3,33 @@
 #include "string_ex.h"
 
 #include <stdbool.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define GROUP_SIZE 3
-#define NUM_ITEMS 52
+#define NUM_ITEMS  52
 
-u8 priority(unsigned char c) {
+u8 priority(unsigned char c)
+{
     // Lowercase priority
     if (c >= 'a' && c <= 'z')
-	return c - 'a' + 1;
+        return c - 'a' + 1;
     if (c >= 'A' && c <= 'Z')
-	return c - 'A' + 27;
+        return c - 'A' + 27;
 
     printf("Not a valid character: '%c'", c);
     exit(EXIT_FAILURE);
 }
 
-unsigned char mispacked_item(const char *comp_a, const char *comp_b, size_t size)
+unsigned char mispacked_item(const char *comp_a, const char *comp_b,
+                             size_t size)
 {
     // Go through the items in the first backpack compartment
     for (size_t i = 0; i < size; ++i) {
-	char *wrongly_packed = strnchr(comp_b, comp_a[i], size);
-	if (wrongly_packed != NULL)
-	    return *wrongly_packed;
+        char *wrongly_packed = strnchr(comp_b, comp_a[i], size);
+        if (wrongly_packed != NULL)
+            return *wrongly_packed;
     }
 
     printf("A rucksack was packed correctly???");
@@ -37,22 +39,22 @@ unsigned char mispacked_item(const char *comp_a, const char *comp_b, size_t size
 void init_all_fields(struct Bitfield *fields, size_t num)
 {
     for (size_t i = 0; i < num; ++i) {
-	bitfield_init(&fields[i], NUM_ITEMS);
+        bitfield_init(&fields[i], NUM_ITEMS);
     }
 }
 
 void deinit_all_fields(struct Bitfield *fields, size_t num)
 {
     for (size_t i = 0; i < num; ++i) {
-	bitfield_deinit(&fields[i]);
+        bitfield_deinit(&fields[i]);
     }
 }
 
 void add_priorities_to_field(struct Bitfield *field, char *string, size_t len)
 {
     for (size_t i = 0; i < len; ++i) {
-	u8 prio = priority((unsigned char) string[i]);
-	bitfield_set_bit(field, prio - 1, true);
+        u8 prio = priority((unsigned char) string[i]);
+        bitfield_set_bit(field, prio - 1, true);
     }
 }
 
@@ -60,34 +62,35 @@ bool next_groups_item(FILE *file, u8 *prio)
 {
     struct Bitfield found_items[GROUP_SIZE];
     init_all_fields(found_items, GROUP_SIZE);
-        
+
     char *line = NULL;
     size_t line_len = 0;
     for (size_t i = 0; i < GROUP_SIZE; ++i) {
-	ssize_t read = getline(&line, &line_len, file);
-	if (read == -1) {
-	    deinit_all_fields(found_items, GROUP_SIZE);
-	    free(line);
-	    return false;
-	}
+        ssize_t read = getline(&line, &line_len, file);
+        if (read == -1) {
+            deinit_all_fields(found_items, GROUP_SIZE);
+            free(line);
+            return false;
+        }
 
-	add_priorities_to_field(&found_items[i], line, read - 1);
+        add_priorities_to_field(&found_items[i], line, read - 1);
     }
     free(line);
 
     // Find an item they all have in common
-    struct Bitfield common_items = bitfield_bitwise_and(found_items, GROUP_SIZE);
+    struct Bitfield common_items =
+        bitfield_bitwise_and(found_items, GROUP_SIZE);
     ssize_t first_common = bitfield_first_high_bit_pos(&common_items);
 
     deinit_all_fields(found_items, GROUP_SIZE);
     bitfield_deinit(&common_items);
 
     if (first_common != -1) {
-	*prio = first_common + 1;
-	return true;
+        *prio = first_common + 1;
+        return true;
     }
     else {
-	return false;
+        return false;
     }
 }
 
@@ -95,8 +98,8 @@ int main()
 {
     FILE *file = fopen("input/day3.txt", "r");
     if (file == NULL) {
-	printf("Unable to open input file");
-	return 1;
+        printf("Unable to open input file");
+        return 1;
     }
 
     char *line = NULL;
@@ -104,9 +107,9 @@ int main()
     ssize_t read;
     u32 priority_sum = 0;
     while ((read = getline(&line, &line_len, file)) != -1) {
-	size_t half = read / 2;
-	unsigned char mispacked = mispacked_item(line, line + half, half);
-	priority_sum += priority(mispacked);
+        size_t half = read / 2;
+        unsigned char mispacked = mispacked_item(line, line + half, half);
+        priority_sum += priority(mispacked);
     }
 
     printf("Sum of mispacked item priorities: %d\n", priority_sum);
@@ -117,10 +120,11 @@ int main()
     u8 prio;
     priority_sum = 0;
     while (next_groups_item(file, &prio)) {
-	priority_sum += prio;
+        priority_sum += prio;
     }
 
-    printf("The priorities of all groups' identifying items: %d\n", priority_sum);
+    printf("The priorities of all groups' identifying items: %d\n",
+           priority_sum);
 
     fclose(file);
     return EXIT_SUCCESS;
